1f800c1662dba94f9d9667cc6250fa9a23ed95da
AkbarFelda <akbarfelda65@gmail.com>
2025-05-01 18:09:09 +0700

    style: create 1 ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,44 @@
-import Image from "next/image";
+import { FiArrowRight } from 'react-icons/fi';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <main className="flex min-h-screen flex-col-3 items-center justify-between p-25">
+      <div className='gap-y-5 flex flex-col'>
+        <h1 className="text-4xl font-gabaritobold text-gray-500">Frontend Dev</h1>
+        <h1 className="text-4xl font-gabaritosemibold text-amber-50 max-w-2xs">Muhammad Akbar Felda</h1>
+        <div className="bg-amber-200 p-0.5"></div>
+        <p className="text-base font-gabaritoreguler text-gray-500 max-w-2xs">
+          Experienced in building responsive user interfaces using Flutter and React. Skilled in managing state with GetX. Focused on clean code, UI/UX principles, and effective team collaboration to create efficient and engaging applications.
+        </p>
+        <a href="#" className="text-amber-200 flex items-center space-x-2">
+          <span>Let's talk</span>
+          <FiArrowRight className="text-amber-200" />
+        </a>
+      </div>
+      <div className='content-center flex flex-col items-center'>
+        <img
+          src="/images/akbar.png"
+          alt="photo"
+          className="rounded- w-51 h-72 -translate-x-20 object-cover border-4 border-amber-200 shadow-lg shadow-gray-500 "
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      </div>
+      <div className='flex flex-col gap-y-20 text'>
+        <div className='text-base font-gabaritosemibold text-gray-500 flex flex-col text-end'>
+          <span>Years of</span>
+          <span>Experience</span>
+          <p className='text-2xl font-gabaritobold text-white'>2+</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className='text-base font-gabaritosemibold text-gray-500 flex flex-col text-end'>
+          <span>Complete</span>
+          <span>Project</span>
+          <p className='text-2xl font-gabaritosemibold text-white'>2+</p>
+        </div>
+        <div className='text-base font-gabaritosemibold text-gray-500 flex flex-col text-end'>
+          <span>Ongoing</span>
+          <span>Project</span>
+          <p className='text-2xl font-gabaritobold text-white~'>2+</p>
+        </div>
+      </div>
+    </main>
   );
 }
