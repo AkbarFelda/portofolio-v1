32afa03725217f15f6a602061050f986520c88ba
AkbarFelda <akbarfelda65@gmail.com>
2025-05-18 21:23:56 +0700

    add marque social link

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -3,27 +3,30 @@ import { FiArrowRight } from "react-icons/fi";
 import { Spotlight } from "./ui/Spotlight";
 import FlipImageCard from "./ui/FlipImageCard";
 
+
 const Hero = () => {
   return (
     <div className="relative min-h-screen grid grid-cols-1 md:grid-cols-3 gap-10 items-center justify-center py-20 px-10">
       <div className="absolute top-0 left-0 right-0 bottom-0 z-0">
         <Spotlight
-          className="-top-40 -left-10 md:-left-32 md-top-20 h-screen" fill="white"
+          className="-top-40 -left-10 md:-left-32 md-top-20 h-screen"
+          fill="white"
         />
         <Spotlight
-          className="top-10 left-full h-[80vh] w-[50vw]" fill="purple"
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="flex flex-col items-center md:items-start">
-        <h1 className="text-5xl font-gabaritobold text-gray-500 whitespace-nowrap text-center md:text-left mt-4 md:mb-8 z-10">
+      <div className="flex flex-col items-center md:items-start space-y-3">
+        <h1 className="text-5xl font-gabaritobold text-gray-500 text-nowrap text-center md:text-left z-10">
           Frontend Developer
         </h1>
         <h1 className="text-5xl font-gabaritosemibold text-amber-50 max-w-xs text-center md:text-left z-10">
           Muhammad Akbar Felda
         </h1>
-        <div className="bg-amber-200 p-0.5 w-24 my-3 mx-auto md:mx-0 z-10"></div>
-        <p className="text-lg font-gabaritoreguler text-gray-500 max-w-xs text-center md:text-left">
+        <div className="bg-amber-200 p-0.5 w-20 mx-auto md:mx-0 z-10"></div>
+        <p className="text-base font-gabaritoreguler text-gray-500 text-center md:text-left max-w-xs">
           Experienced in building responsive user interfaces using Flutter and
           React. Skilled in managing state with GetX. Focused on clean code,
           UI/UX principles, and effective team collaboration to create efficient
@@ -31,7 +34,7 @@ const Hero = () => {
         </p>
         <a
           href="#contact"
-          className="text-amber-200 flex items-center space-x-2 mt-5 justify-center md:justify-start z-10"
+          className="text-amber-200 flex items-center space-x-2 justify-center md:justify-start z-10 mt-2"
         >
           <span>Let's talk</span>
           <FiArrowRight className="text-amber-200" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import Hero from "./components/Hero";
 import { FaHome, FaUserAlt, FaCodeBranch, FaEnvelope, FaCertificate } from "react-icons/fa";
 import { FloatingNav } from "./components/ui/Navbar";
-import SocialIcons from "./components/SocialLinks";
+import MarqueeSocial from "./components/MarqueeSocial";
 
 export default function Home() {
   return (
     <main className="relative justify-center items-center flex flex-col">
-      <div className="relative max-w-7xl w-full">
+      <div className="relative max-w-8xl w-full">
         <FloatingNav
           navItems={[
             { name: 'Home', link: '/', icon: <FaHome /> },
             { name: 'About', link: '/about', icon: <FaUserAlt /> },
             { name: 'Projects', link: '/projects', icon: <FaCodeBranch /> },
             { name: 'Certificate', link: '/certif', icon: <FaCertificate /> },
-            { name: 'Contact', link: '/contact', icon: <FaEnvelope /> },
+            { name: 'Experience', link: '/contact', icon: <FaEnvelope /> },
           ]}
         />
         <Hero />
-        <SocialIcons/>
+        <MarqueeSocial />
       </div>
     </main>
   )
